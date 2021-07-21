2b21e290c5db28e7a18b34f5f9af1e8042af1ebe
Bhavana Chivukula <bchivukula1999@gmail.com>
2021-07-21 20:12:54 +0530

    Initial commit hello world

---
+++ demo.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Challenge1
+{
+    public class Program
+    {
+        public static void Main(string[] args){
+        Console.WriteLine("Hello world");
+        Console.ReadKey();
+        }
+    }
+    
+}
