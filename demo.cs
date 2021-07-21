cd7fd66bafff88c2d9acb3b628d4926226ddf142
Bhavana Chivukula <bchivukula1999@gmail.com>
2021-07-21 20:18:16 +0530

    Addition feature is added in feature/addition

--- demo.cs
+++ demo.cs
@@ -5,7 +5,14 @@ namespace Challenge1
     {
         public static void Main(string[] args){
         Console.WriteLine("Hello world");
+        add();
         Console.ReadKey();
+        }
+        static void add(){
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            int c=a+b;
+            Console.WriteLine("Addition program of a + b is"+c);
         }
     }
     
